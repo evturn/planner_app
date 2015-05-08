ff41613d895d87bd1680a7d72e05c0a306804cd0
Jeremy Alexander <jdoubleua@gmail.com>
2015-05-07 22:37:10 -0400

    added  lots

--- public/js/models/job-model.js
+++ public/js/models/job-model.js
@@ -1,4 +1,23 @@
 var app = app || {};
 
 
-app.JobModel = Backbone.Model.extend({});
+app.JobModel = Backbone.Model.extend({
+    defaults: {
+        title: "Poseboards",
+        profitability: 100,
+        hourly: 18,
+        desc: "Simple Website"
+    },
+
+    validate: function(attrs){
+        if(!attrs.title)
+            return "title is required!";
+    }
+
+});
+
+
+
+
+
+
--- public/js/views/job-view.js
+++ public/js/views/job-view.js
@@ -1,28 +1,32 @@
 var app = app || {};
 
 app.JobView = Backbone.View.extend({
-    el:'#main-name',
-    JobTemplate: _.template($('#main-template').html()),
+
     initialize: function () {
-        console.log('JobView Created');
 
-        this.counter = 0;
         this.render();
-    },
-    events: {
-        'click #nav-add-job': 'addjob'
-    },
-    render: function(){
-        $(this.el).append("<ul> <li>Hello world </li></ul>");
-        $(this.el).append("<button id='nav-add-job'>Add Job</button>");
+        console.log("New JobView Instance created!");
+
 
     },
-    addjob: function(){
-        this.counter++;
-        $('ul', this.el).append("<li>Job added "+ this.counter+ "</li>");
-        console.log('You have added a job');
+
+    events: {
+        'click #billables': "OnBillablesClicked"
+    },
+
+    render: function(){
+
+        console.log("JobView Render function created!");
+
+    },
+
+
+
+    OnBillablesClicked: function(){
+        console.log("Billables clicked");
     }
 
 });
 
-app.jobview = new app.JobView();
+var jobview = new app.JobView();
+jobview.render();
