23f13039a896ff850e57a70afb81b29ad2ef6559
Evan Turner <evturn@gmail.com>
2015-05-08 01:04:00 -0400

    events created to render sidebar views

--- public/js/views/job-view.js
+++ public/js/views/job-view.js
@@ -4,15 +4,15 @@ app.JobView = Backbone.View.extend({
     el: "#app-right-panel",
     billablesTemplate: _.template($('#billables-template').html()),
     events: {
-        'click #billables': "render"
+        'click #billables' : 'showBillables',
+        'click #jobs'      : 'showJobs',
+        'click #todos'     : 'showTodos',
+        'click #history'   : 'showHistory',
     },
-    initialize: function() {
-        console.log('i\'ve been inited');
-    },
-    render: function(){
+    showBillables: function(){
         console.log("New JobView Instance created!");
         var model = new app.JobModel({title: 'modelskies'});
         $('.main-container').html(this.billablesTemplate(model.toJSON()));
         return this;
-    }
+    },
 });
